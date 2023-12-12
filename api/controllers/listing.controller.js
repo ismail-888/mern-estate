4e2a896b7433de90b9c716090780ecf83b2d026a
Ismail Bakhach <bachach59@gmail.com>
2023-12-12 13:48:39 +0200

    create update listing api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,6 +1,5 @@
-import Listing from '../models/listing.model.js';
-import { errorHandler } from '../utils/error.js';
-
+import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
@@ -11,22 +10,41 @@ export const createListing = async (req, res, next) => {
   }
 };
 
-export const deleteListing =async(req,res,next)=>{
-  const listing=await Listing.findById(req.params.id);
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
 
-  if(!listing){
-    return next(errorHandler(404,'Listing not found!'))
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found!"));
   }
 
-  if(req.user.id !== listing.userRef){
-    return next(errorHandler(401,'You can only delete your own listing!'))
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only delete your own listing!"));
   }
 
   try {
     await Listing.findByIdAndDelete(req.params.id);
-    res.status(200).json('Listing has been deleted')
+    res.status(200).json("Listing has been deleted");
   } catch (error) {
-    next(error)
+    next(error);
   }
+};
 
-}
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "listing not found!"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only update your own listing!"));
+  }
+  try {
+    const updatingListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatingListing);
+  } catch (error) {
+    next(error);
+  }
+};
